195d46472c9a1e4badb1d992235bca3e378bda86
LesnoyPudge <chuykin.a01@gmail.com>
2025-07-10 21:48:04 +0500

    update

--- src/router/pages/NotFoundPage/NotFoundPageView/NotFoundPageView.tsx
+++ src/router/pages/NotFoundPage/NotFoundPageView/NotFoundPageView.tsx
@@ -1,11 +1,23 @@
+import { Router } from '@/src/router';
+import { createStyles } from '@/src/utils';
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 
+
+
+const styles = createStyles({
+    wrapper: 'flex size-full flex-col items-center justify-center gap-5',
+});
 
 
 export const NotFoundPageView: FC = () => {
     return (
-        <>
-            NotFoundPageView
-        </>
+        <div className={styles.wrapper}>
+            <div>Страница не найдена.</div>
+
+            <Link to={Router.Pages.Main.path}>
+                <>Вернуться на главную</>
+            </Link>
+        </div>
     );
 };
